24c4fe013073cd4f646e1de18813fe0fc82cbfc6
Matan Hamilis <matan.hamilis@gmail.com>
2022-03-05 15:25:54 +0200

    change to latex, finish lecture 1

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-pandoc \
-    HEADER.yaml README.md lectures/lecture_01.md TRAILER.md  \
-    -o summary.pdf \
-    --bibliography bibliography.bib \
-    -C  \
-    --toc \
-    --pdf-engine=xelatex
